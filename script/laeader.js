a7632d210b3b3f7f63f56fb2c35108abd7890a89
ToddChina <493661223@qq.com>
2016-04-16 13:40:56 +0800

    del

--- script/laeader.js
+++
@@ -1,7 +0,0 @@
-/**
- *
- * @authors Your Name (you@example.org)
- * @date    2016-04-12 12:37:44
- * @version $Id$
- */
-
--- script/online-pay----.js
+++
@@ -1,7 +0,0 @@
-/**
- *
- * @authors Your Name (you@example.org)
- * @date    2016-04-09 01:56:20
- * @version $Id$
- */
-
